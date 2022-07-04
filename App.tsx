84d0a46801af379ce4084cd4aae613cf34c6ad68
Edwards Moses <edwardsmoses3@gmail.com>
2022-07-04 02:44:23 +0100

    Note taking

--- App.tsx
+++ App.tsx
@@ -1,32 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
-
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  Canvas,
+  Fill,
+  useTouchHandler,
+  Path,
+  Skia,
+  useCanvasRef,
+} from '@shopify/react-native-skia';
 
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -35,16 +23,40 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const canvasRef = useCanvasRef();
+
+  const path = Skia.Path.Make();
+
+  // Touch handler
+  const touchHandler = useTouchHandler({
+    onStart: touch => {
+      const {x, y} = touch;
+      path.moveTo(x, y);
+    },
+    onActive: touch => {
+      const {x, y} = touch;
+      path.lineTo(x, y);
+    },
+    onEnd: ({}) => {},
+  });
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      
+      <Canvas style={styles.canvas} ref={canvasRef} onTouch={touchHandler}>
+        <Fill color="white" />
+        <Path path={path} color="lightblue" style={'stroke'} strokeWidth={3} />
+      </Canvas>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
- 
+  canvas: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#FEFEFE',
+  },
 });
 
 export default App;
