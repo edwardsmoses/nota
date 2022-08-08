caea90f3445f77a6946e5ef546aac2450a05620c
Edwards Moses <edwardsmoses3@gmail.com>
2022-08-08 22:26:26 +0100

    test still not working: scrap next

--- __tests__/Toolbox.test.tsx
+++ __tests__/Toolbox.test.tsx
@@ -19,30 +19,17 @@ describe('Toolbox Tests', () => {
   it('should add a New page when the add button is pressed', () => {
     const {getByTestId} = render(<Toolbox />);
 
-    let currentPagesLength = 0;
-
-    //get the current page length
-    try {
-      currentPagesLength = (
-        JSON.parse(storage.getString(NOTE_PAGES_KEY) || '') as Array<NotePage>
-      ).length;
-    } catch (error) {}
-
     const addButton = getByTestId('AddPage.Button');
+    const pageIdLabel = getByTestId('AddPage.PageId');
+
+    console.log(pageIdLabel.props);
 
     //fire the button pressed
     fireEvent.press(addButton);
 
-    let newPagesLength = 0;
-
-    //get the current page length
-    try {
-      newPagesLength = (
-        JSON.parse(storage.getString(NOTE_PAGES_KEY) || '') as Array<NotePage>
-      ).length;
-    } catch (error) {}
+    console.log(pageIdLabel.props);
 
     //assert 2: that a new page was added..
-    expect(newPagesLength).toEqual(currentPagesLength + 1);
+    // expect().toEqual(c);
   });
 });
--- src/components/Toolbox.tsx
+++ src/components/Toolbox.tsx
@@ -1,47 +1,53 @@
 import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
-import {FAB, Portal, Provider} from 'react-native-paper';
+import {FAB, Portal, Provider, Text} from 'react-native-paper';
 import {COLORS} from '@utils/colors';
 import {useNote} from '@storage/storage';
 
 export const Toolbox = () => {
   const [open, setOpen] = useState(false);
 
-  const {addNewPage} = useNote();
+  const {addNewPage, currentPage} = useNote();
 
   return (
-    <Provider>
-      <Portal>
-        <FAB.Group
-          open={open}
-          visible={true}
-          color={COLORS.LIGHT_BLACK}
-          icon="tools"
-          fabStyle={styles.groupIcon}
-          actions={[
-            {
-              icon: 'plus',
-              onPress: addNewPage,
-              color: COLORS.LIGHT_BLACK,
-              testID: 'AddPage.Button',
-            },
-          ]}
-          onStateChange={state => {
-            setOpen(state.open);
-          }}
-          onPress={() => {
-            if (open) {
-              // do something if the speed dial is open
-            }
-          }}
-        />
-      </Portal>
-    </Provider>
+    <>
+      <Text style={styles.hiddenText} testID="AddPage.PageId">{currentPage?.id}</Text>
+      <Provider>
+        <Portal>
+          <FAB.Group
+            open={open}
+            visible={true}
+            color={COLORS.LIGHT_BLACK}
+            icon="tools"
+            fabStyle={styles.groupIcon}
+            actions={[
+              {
+                icon: 'plus',
+                onPress: addNewPage,
+                color: COLORS.LIGHT_BLACK,
+                testID: 'AddPage.Button',
+              },
+            ]}
+            onStateChange={state => {
+              setOpen(state.open);
+            }}
+            onPress={() => {
+              if (open) {
+                // do something if the speed dial is open
+              }
+            }}
+          />
+        </Portal>
+      </Provider>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   groupIcon: {
     backgroundColor: COLORS.LIGHT_GREY,
+  },
+  hiddenText: {
+    display: 'none',
   },
 });
